dfb35e8316937baaaedb22846f0f2813241e330c
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-05-21 17:45:20 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PersonalPage
+
+![Alt text](Sin%20t%C3%ADtulo-2023-05-21-1317.png)
+
+![Alt text](Screenshot_2.png)
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <h1>Sección nº 1</h1>
+      </section>
+      <section>
+        <h1>Sección nº 2</h1>
+      </section>
+      <section>
+        <h1>Sección nº 3</h1>
+      </section>
+      <section>
+        <h1>Sección nº 4</h1>
+      </section>
+      <section>
+        <h1>Sección nº 5</h1>
+      </section>
+      <section>
+        <h1>Sección nº 6</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
